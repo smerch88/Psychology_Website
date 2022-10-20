022944e514a966dddc89c58222a0b659b05ed5f9
smerch88 <34419542+smerch88@users.noreply.github.com>
2022-10-20 13:53:21 +0000

    Deploying to gh-pages from @ smerch88/Psychology_Website@524340d4d48f8067d12e7ab34f78535355090a8c 🚀

---
+++ index.68bf2881.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"vertical",loop:!1,pagination:{el:".swiper-pagination"},scrollbar:{el:".swiper-scrollbar"},autoplay:{delay:5e3}});
+//# sourceMappingURL=index.68bf2881.js.map
---
+++ index.ae56ba1b.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"vertical",loop:!1,pagination:{el:".swiper-pagination"},scrollbar:{el:".swiper-scrollbar"},autoplay:{delay:5e3}});
+//# sourceMappingURL=index.ae56ba1b.js.map

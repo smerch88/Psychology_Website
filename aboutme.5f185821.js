95ecb421ed7f0f6103bde4d0863c3e821982d7b5
smerch88 <34419542+smerch88@users.noreply.github.com>
2022-11-02 11:58:38 +0000

    Deploying to gh-pages from @ smerch88/Psychology_Website@a63de6b554553ba1fe513236627701373eec8f84 🚀

--- aboutme.5f185821.js
+++ aboutme.5f185821.js
@@ -1,2 +1,2 @@
 new Swiper(".swiper",{direction:"vertical",loop:!1,pagination:{el:".swiper-pagination"},scrollbar:{el:".swiper-scrollbar"},autoplay:{delay:5e3}});
-//# sourceMappingURL=index.ae56ba1b.js.map
+//# sourceMappingURL=aboutme.5f185821.js.map
